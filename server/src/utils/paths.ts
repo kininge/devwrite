8bb08c974a3aec8ee2e50b610c3ffe44d341701b
Pritam Kininge <pritam.kininge@icicisecurities.com>
2025-06-12 17:26:21 +0530

    feat: logout done

--- server/src/utils/paths.ts
+++ server/src/utils/paths.ts
@@ -8,4 +8,5 @@ export const ACTIONS = {
 	SIGNUP: "/signup", // Action for user signup
 	LOGIN: "/login", // Action for user login
 	REFRESH_TOKEN: "/refresh-token", // Action for refreshing user tokens
+	LOGOUT: "/logout", // Action for user logout
 };
